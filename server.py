e3be8e8b0ffbd0af5202da66dd4c5a57789c6e12
Olivia Tran <oliviatran99@gmail.com>
2022-05-02 17:12:23 -0700

    added data model

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 """Server for catculator app."""
+from flask import request
 from flask import Flask, render_template
+from importlib_metadata import files
 
 app = Flask(__name__)
 
@@ -35,6 +37,32 @@ def thank_you():
     return render_template('thankyou.html')
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+# sessions https://flask.palletsprojects.com/en/2.1.x/quickstart/#sessions
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         return do_the_login()
+#     else:
+#         return show_the_login_form()
+# url_for('static', filename='style.css')
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if valid_login(request.form['username'],
+#                        request.form['password']):
+#             return log_the_user_in(request.form['username'])
+#         else:
+#             error = 'Invalid username/password'
+#     # the code below is executed if the request method
+#     # was GET or the credentials were invalid
+#     return render_template('login.html', error=error)
+# https://flask.palletsprojects.com/en/2.1.x/patterns/fileuploads/ for uploading files
 if __name__ == "__main__":
     # DebugToolbarExtension(app)
 
