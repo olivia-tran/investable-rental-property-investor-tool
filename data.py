34136a02f24182591bdf5609c5660974047eda0f
Olivia Tran <oliviatran99@gmail.com>
2022-06-06 10:10:42 -0700

    uploaded data.py

---
+++ data.py
@@ -0,0 +1,23 @@
+# quotes from https://www.leapdfw.com/blog/inspirational-quotes-for-real-estate-investors/
+
+QUOTES = [
+    {'Jim Rohn': 'Time is more valuable than money. You can get more money, but you cannot get more time.'},
+    {'Theodore Roosevelt': 'Believe you can and you’re halfway there.'},
+    {'John Jacob Astor': 'Buy on the fringe and wait. Buy land near a growing city! Buy real estate when other people want to sell. Hold what you buy!'},
+    {'Mark Twain': 'Buy land, they aren’t making anymore of it.'},
+    {'Jay Baer': 'The more you know about your customers, the more you can provide to them information that is increasingly useful, relevant, and persuasive.'},
+    {'Warren Buffett': 'Risk comes from not knowing what you’re doing. '},
+    {'Peter Lynch': 'Know what you own, and know why you own it.'},
+    {'Albert Einstein': 'You can never solve a problem with the same kind of thinking that created the problem in the first place.'},
+    {'Mark Twain': 'A person who won’t read has no advantage over one who can’t read.'},
+    {'Warren Buffett': 'In the business world, the rearview mirror is always clearer than the windshield'},
+    {'Theodore Roosevelt': 'Every person who invests in well-selected real estate in a growing section of a prosperous community adopts the surest and safest method of becoming independent, for real estate is the basis of wealth.'},
+    {'Harv Eker': 'Don’t wait to buy real estate, buy real estate and wait.'},
+    {'Robert Kiyosaki': 'Real estate investing, even on a very small scale, remains a tried and true means of building an individual’s cash flow and wealth. '},
+    {'Warren Buffett': 'Price is what you pay. Value is what you get.'},
+    {'Paul Clitheroe': 'Invest in yourself. Your career is the engine of your wealth.'},
+    {'Unknown': 'Just one small positive thought in the morning can change your whole day.'},
+    
+]
+
+
